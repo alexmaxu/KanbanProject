8f97867de140ee9edbce6b81ec4ab6baafd971c3
alex <12alexmaxu@gmail.com>
2024-09-24 11:40:13 +0200

    Initial Commit

---
+++ KanbanProject/KanbanProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanbanProjectApp.swift
+//  KanbanProject
+//
+//  Created by Alex  on 24/9/24.
+//
+
+import SwiftUI
+
+@main
+struct KanbanProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KanbanProjectTests/KanbanProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  KanbanProjectTests.swift
+//  KanbanProjectTests
+//
+//  Created by Alex  on 24/9/24.
+//
+
+import Testing
+@testable import KanbanProject
+
+struct KanbanProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
